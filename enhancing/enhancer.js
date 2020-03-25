9487b849afa8c01da2dd881bbb9c25881df3b1ab
Cristina Edens <edens.cristina@gmail.com>
2020-03-25 19:30:24 -0400

    got the success method to pass 2 total tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,29 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+    succeed,
+    fail,
+    repair,
+    get,
 };
 
 function succeed(item) {
-  return { ...item };
+    if (item.enhancement < 20) {
+        return {...item, enhancement: item.enhancement + 1 };
+    } else if (item.enhancement === 20) {
+        return {...item }
+    } else {
+        item.enhancement += 1;
+        return {...item }
+    }
 }
 
 function fail(item) {
-  return { ...item };
+    return {...item };
 }
 
 function repair(item) {
-  return { ...item };
+    return {...item };
 }
 
 function get(item) {
-  return { ...item };
+    return {...item };
 }
